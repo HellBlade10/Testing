fa7a32e2c90ee154b2a9a951761705c33393562b
Hell Blade <103781988+HellBlade10@users.noreply.github.com>
2025-02-16 09:40:43 +0545

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ let body = document.getElementsByTagName("body");
 btn.addEventListener("click", () => {
     if (body[0].style.backgroundColor == "black"){
         body[0].style.backgroundColor = "white";
-        btn.value = "Dark Mode";
+        body[0].style.color = "black";
+        btn.textContent = "Dark Mode";
     }
     else{
         body[0].style.backgroundColor = "black";
-        btn.value = "Light Mode";
+        body[0].style.color = "white";
+        btn.textContent = "Light Mode";
+        
     }
 });
 
